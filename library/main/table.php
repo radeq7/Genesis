5f653eecfe880ec02ae42e9af5be5217745bfac0
Radeq <Radeq@Radeq-PC.home>
2015-09-02 11:57:54 +0200

    zmiana klasy table
    zmienne do zapisu/odczytu z BD są teraz przechowywane we właściwościach
    obiektu z prefiksem db_

--- library/main/table.php
+++ library/main/table.php
@@ -1,61 +1,61 @@
 <?php
 
 abstract class library_main_table {
+	
 	/**
 	 * Nazwa tabeli w bazie danych
 	 * @var string
 	 */
 	protected $table_name;
+	/**
+	 * Numer id w bazie danych
+	 * @var int
+	 */
 	protected $id = 0;
+	/**
+	 * Nazwa domyślna kolumny 'id' w tabeli
+	 * @var string
+	 */
 	protected $id_name = 'id';
-	protected $dbValues = array();
-	protected $change = array();
 	
-	/**Deleguje pobranie wartości z tabeli bazy danych i zapisuje je do tablicy $dbValues
+	/**
+	 * Deleguje pobranie wartości z tabeli bazy danych i zapisuje je do tablicy $dbValues
 	 * @param int $id nr id w tabeli bazy dancyh
 	 */
 	function load($id){
 		$this->id = $id;
-		$this->dbValues = library_main_objectWatcher::get_model($this);
+		$this->load_variable(library_main_objectWatcher::get_model($this));
 	}
 	
-	/**Deleguje utworzenie nowego wiersza w tabeli bazy danych
-	 * 
+	/**
+	 * Wczytuje zmienne z tablicy i przypisuje je do właściwości obiektu z prefiksem db_
+	 * @param array $var_list
 	 */
-	function create(){
+	function load_variable(array $var_list){
+		foreach ($var_list as $var_name => $value){
+			$var_prefix_name = 'db_' . $var_name;
+			$this->$var_prefix_name = $value;
+		}
+	}
+	
+	/**
+	 * Oznacza obiekt do utworzenie nowego wiersza w tabeli bazy danych
+	 */
+	function markCreate(){
 		library_main_objectWatcher::add_new($this);
 	}
 	
-	/**Ustawia parametr z wartością do zapisu w tabeli bazy danych
-	 * @param string $var nazwa parametru
-	 * @param mixed $value wartość parametru
+	/**
+	 * Oznacza obiekt, aby nie został zaktualizowany w bazie danych
 	 */
-	protected function set_var($var, $value){
-		$this->dbValues[$var] = $value;
-		$this->change[$var] = $value;
-		if ($this->id)
-			$this->dirty();
-	}
-	
-	/**Pobiera wartość parametru o podanej nazwie 
-	 * @param string $var
-	 * @return mixed
-	 */
-	function get_var($var){
-		return $this->dbValues[$var];
-	}
-	
-	/**Oznacza obiekt, aby nie został zaktualizowany w bazie danych
-	 * 
-	 */
-	function clean(){
+	function markClean(){
 		library_main_objectWatcher::clean($this);
 	}
 	
-	/**Oznacza obiekt, aby został usunięty z bazy danych
-	 * 
+	/**
+	 * Oznacza obiekt, aby został usunięty z bazy danych
 	 */
-	function delete(){
+	function markDelete(){
 		library_main_objectWatcher::add_delete($this);
 	}
 	
@@ -71,11 +71,24 @@ abstract class library_main_table {
 		return $this->id;
 	}
 	
+	/**
+	 * Wybiera wszystkie właściwości obiektu z prefiksem db_ i go usuwa
+	 * @return array
+	 */
 	function get_change(){
-		return $this->change;
+		$db_var = get_object_vars($this);
+		foreach ($db_var as $var_name => $value){
+			if ((strpos($var_name, 'db_') === 0))
+				$db_var_without_prefix[substr($var_name, 3)] = $value;
+		}
+		return $db_var_without_prefix;
 	}
 	
-	protected function dirty(){
-		library_main_objectWatcher::add_dirty($this);
+	/**
+	 * Oznacza obiekt, jako do aktualizacji
+	 */
+	protected function markDirty(){
+		if ($this->id)
+			library_main_objectWatcher::add_dirty($this);
 	}
 }
