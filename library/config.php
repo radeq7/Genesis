f8d36e7bb4c3037785a2da330c06db295621b223
Radeq <Radeq@Radeq-I5.home>
2016-07-06 23:37:54 +0200

    fix

--- library/config.php
+++ library/config.php
@@ -11,15 +11,12 @@ appConfig::setConfig('title', 'Genesis Framework');
 // adres email strony
 appConfig::setConfig('email', 'email@genesisframework.pl');
 
-// wyświetlanie błędów
-appConfig::setConfig('error', true);
-
 // dane do domyślnego połączenia z bazą dancyh
-appConfig::setConfig('db_host', '23112.m.tld.pl');
-appConfig::setConfig('db_name', 'baza23112_system_genix3');
-appConfig::setConfig('db_username', 'admin23112_system_genix3');
-appConfig::setConfig('db_pass', 'cqBomu4ayG3UzMQslV6n');
-appConfig::setConfig('db_charset', 'utf8');
+appConfig::setConfig('db_host', '');
+appConfig::setConfig('db_name', '');
+appConfig::setConfig('db_username', '');
+appConfig::setConfig('db_pass', '');
+appConfig::setConfig('db_charset', '');
 
 // domyślna strona logowania
 appConfig::setConfig('login_site', 'index');
